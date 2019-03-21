2a8e59ba4e353277d5eb2c34c414790b4e580533
的macbook 贾书唱 <jay@jiashuchangdeMacBook-Air.local>
2019-03-21 10:25:31 +0800

    Initial Commit

---
+++ widgedDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  widgedDemo
+//
+//  Created by 贾书唱的macbook on 2019/3/21.
+//  Copyright © 2019年 jsc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
